93162008a1c665e1fb81267c20407e8c63efcd3c
Aravind832 <aravindmupparaju@gmailcom>
2025-10-07 23:13:47 +0530

    some more patterns

---
+++ patterns.pu/diamond.py
@@ -0,0 +1,17 @@
+# upper pyramid
+n = 6
+for i in range (1,n+1):
+    for j in range (n-i):
+        print(" ", end = " ")
+    for k in range (2*i-1):
+        print("*", end=" ")
+    print()
+
+# lower pyramis
+for i in range(n-1,0,-1):
+    for j in range (n-i):
+        print(" ", end = " ")
+    for k in range (2*i-1):
+        print("*", end=" ")
+    print()    
+
--- patterns.pu/square.py
+++ patterns.pu/square.py
@@ -8,5 +8,5 @@ n = int(input ("enter the number:"))
 for i in range(n):
         for j in range(1,n+1):
             print(j, end=' ')
-        print()  
+        print()       
 
